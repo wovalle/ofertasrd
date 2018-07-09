b8cf72174a816d71b035d7b74f7ec83d9db750ff
Willy Ovalle <willyovalle16@gmail.com>
2018-07-09 11:05:02 -0400

    feat: adding basic resources

---
+++ functions/src/Resources.ts
@@ -0,0 +1,10 @@
+export default {
+  en: {
+    telegram: {
+      afterStart:
+        "Thank you for using ofertasrd bot. You'll now receive all the great offers in Dominican Republic.",
+      invalidCommand: 'Invalid Command',
+      invalidUser: 'Invalid User',
+    },
+  },
+};
