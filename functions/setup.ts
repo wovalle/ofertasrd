b03536fa7433ea9364ee587179e9c8d4b85f0fff
Willy Ovalle <willyovalle16@gmail.com>
2018-07-10 23:28:35 -0400

    chore: automation of telegram hook + tunnel

---
+++ functions/setup.ts
@@ -0,0 +1,20 @@
+import * as ngrok from 'ngrok';
+import axios from 'axios';
+
+// Setup telegram hook and open ngrok
+async function init() {
+  const url = await ngrok.connect(5000);
+  console.log('Tunnel Url: ', url);
+
+  const hookUrl =
+    'https://api.telegram.org/bot453862718:AAEiWUyYJ0apaWddEzzV781qTcUxT2g9ZKg/setWebhook';
+
+  await axios.post(hookUrl, {
+    url: `${url}/ofertasrd-ca055/us-central1/routes-onTelegramMessage`,
+    max_connections: 5,
+  });
+
+  console.log('ready');
+}
+
+init();
